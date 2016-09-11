7b9c442db5c831d3e5b7fe573be55ad52c529b65
Sam Hurst <sam@Sam-MBP2011.local>
2016-09-11 14:32:16 +1000

    Inital commit this repo, consisting of a config script for the Digital Ocean Ubuntu Trusty VM

---
+++ digitalOcean_UbuntuTrusty.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo apt-get install -y git
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io  | bash -s stable
+source /etc/profile.d/rvm.sh
+rvm install 2.3.1
+gem update --system
+gem install bundler
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.7/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+nvm install 6.1
+sudo apt-get install -y sphinxsearch
+sudo apt-get install -y imagemagick
+sudo apt-get install -y mysql-server-5.6
+sudo apt-get install libmysqlclient-dev (needed for mysql2 gem)
+git clone git://github.com/sharetribe/sharetribe.git
+cd sharetribe
+bundle install
