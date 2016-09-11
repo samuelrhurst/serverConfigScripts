45d925ed4d58142819e028b1daee1c9e39be2e78
Sam Hurst <sam@Sam-MBP2011.local>
2016-09-11 15:57:50 +1000

    Dot seems to work, uncommenteding rvm commands

--- digitalOcean_UbuntuTrusty.sh
+++ digitalOcean_UbuntuTrusty.sh
@@ -5,9 +5,9 @@ sudo apt-get install -y git
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io  | bash -s stable
 . /etc/profile.d/rvm.sh
-# rvm install 2.3.1
-# gem update --system
-# gem install bundler
+rvm install 2.3.1
+gem update --system
+gem install bundler
 #Node
 # curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.7/install.sh | bash
 # export NVM_DIR="$HOME/.nvm"
