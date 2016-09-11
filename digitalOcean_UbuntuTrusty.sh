c374a4436fa434a0e1824e02b35012563864e5ac
Sam Hurst <sam@Sam-MBP2011.local>
2016-09-11 15:39:27 +1000

    Changed source to dot

--- digitalOcean_UbuntuTrusty.sh
+++ digitalOcean_UbuntuTrusty.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y git
 #Ruby
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io  | bash -s stable
-/etc/profile.d/rvm.sh
+. /etc/profile.d/rvm.sh
 # rvm install 2.3.1
 # gem update --system
 # gem install bundler
