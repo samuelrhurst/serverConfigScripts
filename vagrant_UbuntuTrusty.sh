86bdbba7ac940904de29e7ef2803d94b830dd1b8
Sam Hurst <sam@Sam-MBP2011.local>
2016-09-13 21:38:46 +1000

    Added sudos to vagrant config script

--- vagrant_UbuntuTrusty.sh
+++ vagrant_UbuntuTrusty.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-apt-get update
-apt-get upgrade -y
+sudo apt-get update
+sudo apt-get upgrade -y
 #MySQL (libmysqlclient-dev is needed for the mysql2 gem)
-apt-get install -y mysql-server-5.6
-apt-get install -y libmysqlclient-dev
+#-------------------------------------------------------
+sudo apt-get install -y mysql-server-5.6
+# apt-get install -y libmysqlclient-dev
 #Git
-apt-get install -y git
+#---
+sudo apt-get install -y git
 #Ruby
+#----
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io  | bash -s stable
 . /etc/profile.d/rvm.sh
@@ -14,15 +17,16 @@ rvm install 2.3.1
 gem update --system
 gem install bundler
 #Node
+#----
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.7/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 nvm install 6.1
 #apt-get install -y build-essential
 #SphinxSearch
-apt-get install -y sphinxsearch
+sudo apt-get install -y sphinxsearch
 #ImageMagick
-apt-get install -y imagemagick
+sudo apt-get install -y imagemagick
 
 # #Sharetribe Installation
 # #=======================
